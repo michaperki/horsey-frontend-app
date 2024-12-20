f0f2eaa3b103eecfe8188fc3d2d881d1d3ce9ace
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-12-20 14:37:06 -0600

    chore(devX): configure ESLint and Prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import parser from "@babel/eslint-parser";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        requireConfigFile: false, // Do not require a Babel config file
+        ecmaVersion: 2021, // Modern ES syntax
+        sourceType: "module", // Enable ES Modules
+        ecmaFeatures: {
+          jsx: true, // Enable JSX
+        },
+      },
+      globals: globals.browser,
+    },
+    settings: {
+      react: {
+        version: "detect", // Automatically detect the React version
+      },
+    },
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "commonjs" },
+  },
+  pluginReact.configs.flat.recommended,
+];
+
