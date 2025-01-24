ce72e0a85c12feaf3815df36fb7bebf18fe7261d
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2025-01-23 19:38:47 -0600

    chore(ESLint): fix unused vars

--- src/components/StatCard.js
+++ src/components/StatCard.js
@@ -2,7 +2,6 @@
 // src/components/StatCard.js
 
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './StatCard.css';
 
 const StatCard = ({ title, value }) => {
